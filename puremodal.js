f2c94a8b9657260f862d0ddc4ac618b948a5380c
hoto17296 <mail@hoto.me>
2019-02-14 23:10:14 +0900

    refactoring

--- puremodal.js
+++ puremodal.js
@@ -1,19 +1,20 @@
 /*!
- * puremodal.js
+ * puremodal.js v1.0.0
  * https://github.com/hoto17296/puremodal.js
  *
  * Copyright @hoto17296
  * Released under the MIT license
  *
- * Date: 2018-06-01
+ * Since: 2018-06-01
+ * Updated: 2019-02-13
  */
 
 /**
  * class EventEmitter
  */
-var EventEmitter = function() {
+function EventEmitter() {
   this.listeners = {};
-};
+}
 
 EventEmitter.prototype.on = function(event, cb) {
   if (!this.listeners[event]) {
@@ -32,51 +33,42 @@ EventEmitter.prototype.emit = function(event) {
 };
 
 /**
- * class ModalBase extends EventEmitter
+ * class PureModal extends EventEmitter
  */
-var ModalBase = function(elem) {
+function PureModal(elem) {
   EventEmitter.apply(this);
   this.elem = elem;
   this.elem.modal = this;
+  this.elem.style.display = 'none';
   // Bind close event
   var closeElements = this.elem.getElementsByClassName('close');
   for (var i = 0; i < closeElements.length; i++) {
     closeElements[i].addEventListener('click', this.close.bind(this));
   }
-};
+}
+PureModal.prototype = Object.create(EventEmitter.prototype);
 
-ModalBase.prototype = Object.create(EventEmitter.prototype);
-
-ModalBase.prototype.open = function() {
+PureModal.prototype.open = function() {
+  // Show modal
   this.elem.style.display = 'block';
+  // Lock background contents
+  this.bodyScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+  document.body.style.marginTop = '-' + this.bodyScroll + 'px';
+  document.body.style.overflow = 'hidden';
+  // Emit open events
   this.emit.apply(this, ['open']);
 };
 
-ModalBase.prototype.close = function() {
+PureModal.prototype.close = function() {
+  // Hide modal
   this.elem.style.display = 'none';
-  this.emit.apply(this, ['close'].concat(arguments));
-};
-
-/**
- * class WindowModal extends ModalBase
- */
-WindowModal = function() {
-  ModalBase.apply(this, arguments);
-  this.on('open', this.onOpen.bind(this));
-  this.on('close', this.onClose.bind(this));
-};
-
-WindowModal.prototype = Object.create(ModalBase.prototype);
-
-WindowModal.prototype.onOpen = function() {
-  this.bodyScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  document.body.style.marginTop = '-' + this.bodyScroll + 'px';
-  document.body.style.overflow = 'hidden';
-};
-
-WindowModal.prototype.onClose = function() {
+  // Unlock background contents
   document.body.style.marginTop = '';
   document.body.style.overflow = '';
-  document.body.scrollTop = this.bodyScroll
+  document.body.scrollTop = this.bodyScroll;
   document.documentElement.scrollTop = this.bodyScroll;
+  // Emit close events
+  this.emit.apply(this, ['close'].concat(arguments));
 };
